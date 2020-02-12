e2e1d65b52c30161f967396f29b4635e673f4a19
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:47:00 +0000

    Boxes right size, everything is horizontal fix to verticle

--- register.php
+++ register.php
@@ -93,20 +93,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <?php include 'header.php'; ?>
 <body>
-    <div class="wrapper">
+    <div class="container-fluid">
         <h2>Register</h2>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+            <div class="col-lg-3 form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" size="25" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+            <div class="col-lg-3 form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control" size="25" value="<?php echo $password; ?>">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+            <div class="col-lg-3 form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Confirm Password</label>
                 <input type="password" name="confirm_password" class="form-control" size="25" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
