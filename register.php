754e74d0bb1d35eb534b8b90ec8f4b5a26552aee
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:07:17 +0000

    Removed head to include 'header.php'

--- register.php
+++ register.php
@@ -91,15 +91,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
+<?php include 'header.php'; ?>
 <body>
     <div class="wrapper">
         <h2>Register</h2>
