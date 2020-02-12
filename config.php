e4b01b072c9fa2f39aa14868cc55195a54d6c0c5
James Harvey <jharvey9261@gmail.com>
2020-02-12 10:40:46 +0000

    Connection to mysql database config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "root";
+$db_name = "myDB";
+
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+
+
+
+
+
+
+
+ ?>
