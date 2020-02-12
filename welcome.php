281c413d0c7930080e06691f0d990915906c05b6
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:07:11 +0000

    Removed head to include 'header.php'

--- welcome.php
+++ welcome.php
@@ -11,14 +11,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
+<?php include 'header.php'; ?>
 <body>
     <div class="page-header">
         <h1>Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
