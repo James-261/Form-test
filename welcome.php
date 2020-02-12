25f798cbd43bdec752e3644c016733f71418f885
James Harvey <jharvey9261@gmail.com>
2020-02-12 12:20:38 +0000

    Welcome page with redirect back to login.

---
+++ welcome.php
@@ -0,0 +1,25 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect them to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <div class="page-header">
+        <h1>welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
+    </div>
+    <p>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
