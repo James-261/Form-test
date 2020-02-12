015bc4a7571d60757fed9e4cc818b17106014034
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:08:02 +0000

    Header file for general styling of page

---
+++ header.php
@@ -0,0 +1,9 @@
+
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: left; }
+    </style>
+</head>
