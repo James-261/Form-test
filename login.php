f6e92ba445dc39baf71fd46618bc823373d014f6
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:16:34 +0000

    Input text size 25 Input boxes still fill page

--- login.php
+++ login.php
@@ -96,12 +96,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                <label>Username</label>
-               <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+               <input type="text" name="username" class="form-control" size="25" value="<?php echo $username; ?>">
                <span class="help-block"><?php echo $username_err; ?></span>
            </div>
            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                <label>Password</label>
-               <input type="password" name="password" class="form-control">
+               <input type="password" name="password" class="form-control" size="25">
                <span class="help-block"><?php echo $password_err; ?></span>
            </div>
            <div class="form-group">
