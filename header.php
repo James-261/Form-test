83ea5900cad2a254af159d91e6aa9f57fc36893e
James Harvey <jharvey9261@gmail.com>
2020-02-12 15:46:11 +0000

    Added padding to inputs

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: left; }
+        body{ font: 14px sans-serif; text-align: left; padding-right: 800px; grid-column: 1;}
     </style>
 </head>
