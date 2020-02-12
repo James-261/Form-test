6c81220ea19469dcf3aa105c8a1fe685419ea990
James Harvey <jharvey9261@gmail.com>
2020-02-12 12:21:15 +0000

    logout page, stops session and redirect to login.php

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+// Initialize the session
+session_start();
+ 
+$_SESSION = array();
+
+// Destroy the session.
+session_destroy();
+
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
