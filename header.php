06379ec0e4ac6393e582f385e6ebad6aa01523bd
James Harvey <jharvey9261@gmail.com>
2020-02-13 10:16:52 +0000

    Styled page a bit

--- header.php
+++ header.php
@@ -4,6 +4,9 @@
     <title>Welcome</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; text-align: left; padding-right: 800px; }
+        body {font: 14px sans-serif; text-align: center; background: grey;}
+        h2 {padding-top: 0px; font-size: 40px;}
+        .box {width: 500px; border: 15px solid #A9A9A9; padding: 50px; margin: auto; margin-top: 150px; clear: both;}
+        .grey {background-color: #DCDCDC;}
     </style>
 </head>
