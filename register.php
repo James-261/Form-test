69348db7aeb96ca5c2e07385d77de9e63a1f286c
James Harvey <jharvey9261@gmail.com>
2020-02-13 13:03:51 +0000

    Submits form on enter

--- register.php
+++ register.php
@@ -98,21 +98,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" autocomplete="off">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
-                <input type="text" name="username" class="form-control" size="25" value="<?php echo $username; ?>">
+                <input id="text" type="text" name="username" class="form-control" size="25" value="<?php echo $username; ?>">
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
                 <label>Password</label>
-                <input type="password" name="password" class="form-control" size="25" value="<?php echo $password; ?>">
+                <input id="text" type="password" name="password" class="form-control" size="25" value="<?php echo $password; ?>">
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
                 <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control" size="25" value="<?php echo $confirm_password; ?>">
+                <input id="text" type="password" name="confirm_password" class="form-control" size="25" value="<?php echo $confirm_password; ?>">
                 <span class="help-block"><?php echo $confirm_password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="submit" class="btn btn-primary" value="Submit" id="submit">
                 <input type="reset" class="btn btn-default" value="Reset">
             </div>
         </form>
