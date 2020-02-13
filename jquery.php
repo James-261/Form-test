b8c98913e15c788679d1f842a8b946bf7a637ab6
James Harvey <jharvey9261@gmail.com>
2020-02-13 11:44:31 +0000

    Jquery test file

---
+++ jquery.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+
+  </style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
+
+
+
+</script>
+</head>
+<body>
+  <div>
+  </div>
+</body>
+</html>
