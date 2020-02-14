09ec98262e05911ab49c11b9df6f674b0b5d00f7
James Harvey <jharvey9261@gmail.com>
2020-02-14 10:54:10 +0000

    a

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 // Initialize the session
 session_start();
 
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, if not then redirect them to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
