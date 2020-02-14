1ee3b8a5d0026d1419887b7bbdc3c27da6ba1b4a
James Harvey <60884353+James-261@users.noreply.github.com>
2020-02-14 07:21:08 +0000

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -12,10 +12,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'header.php'; ?>
-<body>
   <style>
-     body {background: #A9A9A9;}
+    body {background: #A9A9A9;}
   </style>
+<body>
     <div class="page-header">
         <h1>Welcome, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
     </div>
